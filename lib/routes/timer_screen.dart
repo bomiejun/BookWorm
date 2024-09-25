5c2be33df50851efee7f15fcc00e6ec0ea2b8507
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-09-25 13:23:29 -0400

    Nagivation Bar + Removed Home Screen

--- lib/routes/timer_screen.dart
+++ lib/routes/timer_screen.dart
@@ -1,21 +1,52 @@
 import 'package:flutter/material.dart';
-
-class TimerScreen extends StatelessWidget {
+class TimerScreen extends StatefulWidget {
   const TimerScreen({super.key});
+
+  @override
+  State<TimerScreen> createState() =>
+      _TimerScreen();
+}
+
+class _TimerScreen
+    extends State<TimerScreen> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('TimerScreen'),
+        title: const Text('i forget if we wanted a title'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('this does nothing :p'),
-          onPressed: () {
-            // Navigate to new screen
-          }
-        )
+      body: const Center(
+        child: Text('hello'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.equalizer),
+            label: 'History',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.timer),
+            label: 'Timer',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flag),
+            label: 'Goals',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_events),
+            label: 'Achievements',
+          ),
+        ],
+        currentIndex: 1,
+        selectedItemColor: const Color.fromARGB(255, 23, 90, 146),
+        onTap: (index) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => const TimerScreen()),
+      );
+        }
       )
     );
   }

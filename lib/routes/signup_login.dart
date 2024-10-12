d4f707ce1e26bc99d08d127107934f4c8860dd0c
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-10-11 20:17:00 -0400

    yuv

--- lib/routes/signup_login.dart
+++ lib/routes/signup_login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'timer_screen.dart';
 
 
 class SignupLogin extends StatelessWidget {
@@ -9,14 +10,24 @@ class SignupLogin extends StatelessWidget {
  Widget build(BuildContext context) {
    return Scaffold(
      appBar: AppBar(
-       title: Text(
-         'Sign Up or Login',
-         style: TextStyle(
-           color: Colors.black,
-           fontSize: 18,
-           fontWeight: FontWeight.bold,
-         )
-       ),
+       title: const Text(
+        'Sign Up or Login',
+        style: TextStyle(
+          color: Color.fromARGB(255, 210, 210, 210),
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        )
+      ),
+     ),
+     body: Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (context) => const TimerScreen()),
+          );
+        }, child: const Text("NO DELETE PLS"), 
+        // don't delete the button until there's a function button for page navigation/loging in
+      ),
      ),
    );
  }

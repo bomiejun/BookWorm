30f84a6895039e59c6b47e851b6d1637c8c5699b
08tzhuang <txmzhuang@gmail.com>
2024-10-17 18:12:04 -0500

    ello

--- lib/routes/signup_login.dart
+++ lib/routes/signup_login.dart
@@ -10,34 +10,60 @@ class SignupLogin extends StatelessWidget {
  @override
  Widget build(BuildContext context) {
    return Scaffold(
-     appBar: AppBar(
-       title: const Text(
-        'Sign Up or Login',
-        style: TextStyle(
-          color: Color.fromARGB(255, 210, 210, 210),
-          fontSize: 18,
-          fontWeight: FontWeight.bold,
-        )
-      ),
-      leading: Container(
-        margin:EdgeInsets.all(10),
-        child: SvgPicture.asset('assets/icons/Bookworm_Logo'),
-        decoration: BoxDecoration(
-          color: Colors.black,
-          borderRadius: BorderRadius.circular(10)
+     appBar: appBar(),
+     body: Column(
+      children: [
+        Container(
+          width: 
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: (){
+                  
+                }, child: const Text("Sign Up"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) => const TimerScreen()),
+                  );
+                }, child: const Text("NO DELETE PLS"), 
+                // don't delete the button until there's a function button for page navigation/loging in
+              )
+            ],
+          ),
         ),
+      ]
+     ),
+   );
+ }
+
+ AppBar appBar() {
+   return AppBar(
+     title: const Text(
+      'Sign Up or Login',
+      style: TextStyle(
+        color: Color.fromARGB(255, 210, 210, 210),
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
       )
-     ),
-     body: Center(
-      child: ElevatedButton(
-        onPressed: () {
-          Navigator.pushReplacement(context,
-          MaterialPageRoute(builder: (context) => const TimerScreen()),
-          );
-        }, child: const Text("NO DELETE PLS"), 
-        // don't delete the button until there's a function button for page navigation/loging in
+    ),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+    centerTitle: true,
+      //child: SvgPicture.asset('assets/icons/Bookworm_Logo.png'),
+    /*actions: [
+      Container(
+      margin:const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.black,
+        borderRadius: BorderRadius.circular(10)
       ),
-     ),
+      child: SvgPicture.asset('assets/icons/Bookworm_Logo'),
+    )
+    ]*/
    );
  }
 }

d2c74e07c0dbd384bd54dc1ab641a977c616abe6
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-10-25 16:51:19 -0400

    ufiabe

--- lib/routes/timer_screen.dart
+++ lib/routes/timer_screen.dart
@@ -14,8 +14,32 @@ class _TimerScreen extends State<TimerScreen> {
       appBar: AppBar(
         title: const Text('i forget if we wanted a title'),
       ),
-      body: const Center(
-        child: Text('hello'),
+      
+      body: Column (
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget> [
+          Center(
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                // Circle
+                Container(
+                  width: 270, // Circle diameter
+                  height: 270,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Color.fromARGB(255, 236, 236, 236), // Circle color
+                  ),
+                ),
+                // TextField on top of the circle
+                const Center(
+                  child: Text("00:00:00",
+                  style: TextStyle(fontSize: 36)),
+                ),
+              ],
+            ),
+          ),
+        ]
       ),
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,

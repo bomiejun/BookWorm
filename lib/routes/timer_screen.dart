a0eee0c39ab72fbf03f8c8827c013835beec9c07
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-11-01 14:46:18 -0400

    app bar

--- lib/routes/timer_screen.dart
+++ lib/routes/timer_screen.dart
@@ -7,13 +7,31 @@ class TimerScreen extends StatefulWidget {
   State<TimerScreen> createState() => _TimerScreen();
 }
 class _TimerScreen extends State<TimerScreen> {
+  Color buttonColor = const Color.fromARGB(255, 210, 190, 124);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('i forget if we wanted a title'),
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget> [
+            Padding(
+              padding: EdgeInsets.only(left: 6),
+              child: Text('BookWorm', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 33), textAlign: TextAlign.left)
+            ),
+            Padding (
+              padding: EdgeInsets.only(right: 6),
+              child: Icon(Icons.play_arrow))
+            ]
+          ),
+        elevation: 10,
+        shadowColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+        toolbarHeight: 70,
+        
       ),
+      backgroundColor: const Color.fromARGB(255, 243, 243, 243),
       
       body: Column (
         mainAxisAlignment: MainAxisAlignment.center,
@@ -28,7 +46,14 @@ class _TimerScreen extends State<TimerScreen> {
                   height: 270,
                   decoration: const BoxDecoration(
                     shape: BoxShape.circle,
-                    color: Color.fromARGB(255, 236, 236, 236), // Circle color
+                    color: Color.fromARGB(255, 250, 250, 250), // Circle color
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color.fromARGB(255, 183, 183, 183), // Shadow color with opacity
+                        spreadRadius: 3, // Spread of the shadow
+                        blurRadius: 7, // Blur radius
+                      ),
+                    ],
                   ),
                 ),
                 // TextField on top of the circle
@@ -53,15 +78,16 @@ class _TimerScreen extends State<TimerScreen> {
                     borderRadius: BorderRadius.circular(8), // Adjust the radius here
                   ),
                   padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12), // Optional padding
+                  backgroundColor: buttonColor
                 ),
                 onPressed: () {
 
                 },
-                child: const Text("End", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: Colors.white)) 
+                child: const Text("End", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20, color: Colors.white)) 
                 
               ),
               const SizedBox(
-                width: 10
+                width: 12
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -72,15 +98,16 @@ class _TimerScreen extends State<TimerScreen> {
                     
                     borderRadius: BorderRadius.circular(8), // Adjust the radius here
                   ),
+                  backgroundColor: buttonColor
                 ),
                 onPressed: () {
 
                 },
-                child: const Icon(Icons.play_arrow, size: 26, color: Colors.white) 
+                child: const Icon(Icons.play_arrow, size: 26, color: Colors.white)
                 
               ),
               const SizedBox(
-                width: 10
+                width: 12
               ),
               ElevatedButton(
                 style: ElevatedButton.styleFrom(
@@ -90,11 +117,12 @@ class _TimerScreen extends State<TimerScreen> {
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(8), // Adjust the radius here
                   ),
+                  backgroundColor: buttonColor
                 ),
                 onPressed: () {
 
                 },
-                child: const Text("Add", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 18, color: Colors.white)) 
+                child: const Text("Add", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20, color: Colors.white)),
                 
               )
             ]

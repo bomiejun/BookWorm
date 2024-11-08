cea7af65de14d16a6c3e077274084c0c963c4e43
bomiejun <116047701+bomiejun@users.noreply.github.com>
2024-11-08 18:34:42 -0500

    Revert "did smth change?"
    This reverts commit 1f78f46bae0e5a2b301a32832b1ddcbea4f76106.

--- lib/routes/signup_login.dart
+++ lib/routes/signup_login.dart
@@ -19,14 +19,13 @@ class SignupLogin extends StatelessWidget {
           height: 200,
           fit: BoxFit.cover, // Optional: Control how the image fits
         ),
-        const SizedBox(height: 20,),
+        const SizedBox(height: 40,),
         usernameText(),
         usernameInput(),
-        const SizedBox(height: 20,),
+        const SizedBox(height: 40,),
         passwordText(),
         passwordInput(),
-        const SizedBox(height: 20,),
-        checkAgreements(),
+        const SizedBox(height: 40,),
         Row(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -49,23 +48,6 @@ class SignupLogin extends StatelessWidget {
       ],
      ),
    );
- }
-
- Row checkAgreements() {
-   return Row(
-        children: [
-          SizedBox(child: Checkbox(value: false, onChanged: (value){},)),
-          Text.rich(
-            TextSpan(children: [
-              TextSpan(text: 'I agree to '),
-              TextSpan(text: 'Terms of Use'),
-              TextSpan(text: ' and '),
-              TextSpan(text: 'Privacy Policy'),
-              //https://www.youtube.com/watch?v=TQMMG39qw0c time 12:07
-            ])
-          )
-        ],
-      );
  }
 
  Container passwordInput() {
